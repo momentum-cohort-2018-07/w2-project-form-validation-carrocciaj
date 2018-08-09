564196471fdff676736db296dcd7c0668bc901d8
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-09 16:51:23 -0400

    done with step one

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 
 document.getElementById('parking-form').addEventListener('submit', function (event) {
     event.preventDefault()
-  
+    var carYear = document.getElementById('car-year').value.trim()
+    var carMake = document.getElementById('car-make')
+    var carModel = document.getElementById('car-model') 
     clearError()
-
+    clearCarError() 
   for (inputField of document.querySelectorAll('input')){
-    if (inputField.value.trim() === '' && inputField.parentElement.classList.value === "input-field"){
+    if(inputField.value.trim() !== '' && inputField.parentElement.classList.value === "input-field"){
+        inputField.parentElement.classList.add('input-valid')
+    } 
+    else if (inputField.value.trim() === '' && inputField.parentElement.classList.value === "input-field"){
         inputField.parentElement.classList.add('input-invalid')
-    } else if(inputField.value.trim() === '' && inputField.parentElement.classList.value === "input-group") {
+    }
+     else if (inputField.parentElement.classList.value === "input-group" && (carYear === '' || carMake === '' || carModel === '')) {
         inputField.parentElement.classList.add('input-invalid')
         inputField.parentElement.parentElement.classList.add('input-invalid')
     }
+    else if(inputField.value.trim() !== '' && inputField.parentElement.classList.value === "input-group"){
+        inputField.parentElement.classList.add('input-valid')
+        inputField.parentElement.parentElement.classList.add('input-valid')
   }
-  })
+  }
+})
 
 function clearError (){
     var field = document.querySelectorAll('.input-field')
     for (error of field){
-        error.classList.remove('input-invalid')
+        // console.log(error.id)
+        if (error.id !== 'car-field'){
+         error.classList.remove('input-invalid')
+         error.classList.remove('input-valid')
+        }    
     }
+}
+
+function clearCarError() {
+    var field = document.querySelectorAll('.input-group')
+    for (error of field){
+        // console.log(error)
+         error.classList.remove('input-invalid')
+        // error.parentElement.classList.remove('input-invalid')
+}
 }
