ad181d8e012d9a438a2d410afd410af9fea1768c
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-09 14:49:10 -0400

    added logic to add errors when fields are empty on removal of erros the car div act funny and removes the error every time even if it is not resolved.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Add validation!');
+
+document.getElementById('parking-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+  
+    clearError()
+
+  for (inputField of document.querySelectorAll('input')){
+    if (inputField.value.trim() === '' && inputField.parentElement.classList.value === "input-field"){
+        inputField.parentElement.classList.add('input-invalid')
+    } else if(inputField.value.trim() === '' && inputField.parentElement.classList.value === "input-group") {
+        inputField.parentElement.classList.add('input-invalid')
+        inputField.parentElement.parentElement.classList.add('input-invalid')
+    }
+  }
+  })
+
+function clearError (){
+    var field = document.querySelectorAll('.input-field')
+    for (error of field){
+        error.classList.remove('input-invalid')
+    }
+}
